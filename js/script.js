f277907741ac74d3202eb27bc2a2aef326a127d9
snoz-an <snoz.andrey94@mail.ru>
2022-12-02 22:18:20 +0300

    add header and footer mob

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 $( document ).ready(function() {
+
+ $('.header_profil').click(function(){
+    $('.headeer_menu_mob').addClass('headeer_menu_mob_active')
+})
+
+$(document).mouseup(function (e) {
+    var container = $("headeer_menu_mob");
+    if (container.has(e.target).length === 0){
+        $('.headeer_menu_mob').removeClass('headeer_menu_mob_active')
+    }
+
+})
 })
